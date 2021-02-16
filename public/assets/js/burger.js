e474d5cf948afe6f04948dbb41b63326ec028bb5
johnniesimpson <johnniesimpson@outlook.com>
2021-02-15 21:16:47 -0500

    Touching up minor syntax errors. Fixed the CSS MIME file path issue.

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,4 +1,4 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+// Wait to attach handlers until the DOM is fully loaded.
 document.addEventListener('DOMContentLoaded', (event) => {
     if (event) {
       console.info('DOM loaded');
